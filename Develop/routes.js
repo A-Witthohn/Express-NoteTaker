70d65b0859c60de68c0dc76d084b104706679c6a
Aaron <awitthohn@yahoo.com>
2023-07-18 19:44:01 -0400

    created path for './' , working in insomnia

--- Develop/routes.js
+++ Develop/routes.js
@@ -3,8 +3,14 @@ const path = require('path');
 
 const router = express.Router();
 
+//gets '/notes' route
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'Develop/public', 'notes.html'));
 });
+
+// gets '/' route
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Develop/public', 'index.html'));
+  });
 
 module.exports = router;
