808bfbe99d0386127aefa54908b916689c312e99
Aaron <awitthohn@yahoo.com>
2023-07-18 19:25:47 -0400

    styling issue fixed

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'Develop/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 app.listen(PORT, () => {
