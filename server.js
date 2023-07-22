3e49ce47b856ce6259eb670a270d10bf86ff0326
Aaron <awitthohn@yahoo.com>
2023-07-21 21:25:24 -0400

    changed structure to match removing all contents from develop

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/Develop/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(routes);
 
 app.post('/api/notes', (req, res) => {
