16f5fa1184fb8b2ea1e2e58d177bfffe99ccabb2
Aaron <awitthohn@yahoo.com>
2023-07-21 21:34:14 -0400

    adjusted page so that /notes appears on deployed

--- routes.js
+++ routes.js
@@ -5,17 +5,17 @@ const router = express.Router();
 
 //gets '/notes' route
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '/public', 'notes.html'));
+  res.sendFile(path.join(__dirname, '/public/', 'notes.html'));
 });
 
 // gets '/' route
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', '/public', 'index.html'));
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
   });
 
 // catchs all other routes and redirects to root
   router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '/public', 'index.html'))
+  res.sendFile(path.join(__dirname, '/public', 'index.html'))
   });
 
 module.exports = router;
